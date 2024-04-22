2614f2b7ac8387183c6791506c53588b32e02907
aajmorgan <ajmsd6@gmail.com>
2024-04-22 15:31:22 -0400

    remove encrypted file

--- encrypt.py
+++ encrypt.py
@@ -2,7 +2,9 @@ import cryptography
 from cryptography.fernet import Fernet
 from cryptography.hazmat.primitives.kdf.scrypt import Scrypt
 
+import os
 import base64
+import pathlib
 
 def make_key(salt: bytes, password: str):
     kdf = Scrypt(salt=salt, length=32, n=2**14, r=8, p=1)
@@ -21,6 +23,7 @@ def encrypt_file_algo(encrypt_file, key) -> None:
     with open(encrypt_file+".box", "wb") as outfile:
         outfile.write(encrypted)
     # TODO: delete old file?, or just overwrite 
+    os.remove(pathlib.Path(encrypt_file))
         
 
 def decrypt_file_algo(decrypt_file: str, key) -> bool:
@@ -38,4 +41,5 @@ def decrypt_file_algo(decrypt_file: str, key) -> bool:
     with open(decrypt_file[:-len('.box')], "wb") as outfile:
         outfile.write(decrypted)
 
+    os.remove(pathlib.Path(decrypt_file))
     return True
