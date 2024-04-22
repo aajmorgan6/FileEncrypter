133fd514d4f9ebff8a925607afa5e50579215010
dev <dev@AJs-MacBook-Pro-2.local>
2024-04-21 20:53:52 -0400

    encryption and decryption

---
+++ encrypt.py
@@ -0,0 +1,42 @@
+import cryptography
+from cryptography.fernet import Fernet
+from cryptography.hazmat.primitives.kdf.scrypt import Scrypt
+
+import base64
+import getpass
+
+def make_key(salt: bytes, password: str):
+    kdf = Scrypt(salt=salt, length=32, n=2**14, r=8, p=1)
+    derived = kdf.derive(password.encode())
+    return base64.urlsafe_b64encode(derived)
+
+def encrypt_file_algo(encrypt_file, key) -> None:
+    '''
+    Key will come from both password and seed
+    '''
+    f = Fernet(key)
+    with open(encrypt_file, "rb") as infile:
+        to_encrypt = infile.read()
+
+    encrypted = f.encrypt(to_encrypt)
+    with open(encrypt_file+".box", "wb") as outfile:
+        outfile.write(encrypted)
+    # TODO: delete old file?, or just overwrite 
+        
+
+def decrypt_file_algo(decrypt_file: str, key) -> bool:
+
+    f = Fernet(key)
+    with open(decrypt_file, "rb") as infile:
+        to_decrypt = infile.read()
+
+    try:
+        decrypted = f.decrypt(to_decrypt)
+    except cryptography.fernet.InvalidToken:
+        print("Invalid token")
+        return False 
+    
+    with open(decrypt_file[:-len('.box')], "wb") as outfile:
+        outfile.write(decrypted)
+
+    return True
--- gui.py
+++ gui.py
@@ -2,7 +2,6 @@ import os
 import sys
 import base64
 import pathlib
-from hashlib import sha256
 from random import randrange
 
 from PySide6.QtCore import QRect, QSize, Qt
@@ -13,6 +12,7 @@ from PySide6.QtWidgets import (QApplication, QDial, QGridLayout, QHBoxLayout,
                                QVBoxLayout, QWidget, QFileDialog, QLineEdit)
 
 from nacl.secret import SecretBox
+from encrypt import make_key, encrypt_file_algo, decrypt_file_algo
 
 class SecureDial():
     """
@@ -104,7 +104,8 @@ class EncryptWidget(QWidget):
         self.pass_match = QLabel()
         row_2.addWidget(self.pass_match)
 
-        self.dials = [SecureDial() for _ in range(num_dials)]
+        self.num_dials = num_dials
+        self.dials = [SecureDial() for _ in range(self.num_dials)]
 
         row_3 = QHBoxLayout()
         for dial in self.dials:
@@ -128,25 +129,100 @@ class EncryptWidget(QWidget):
         )
         self.file_label.setText(str(response[0]))
         self.file_path = pathlib.Path(str(response[0]))
+        self.file_path_str = str(response[0])
 
     def runEncryption(self):
         if self.pass_1.text() != self.pass_2.text():
             self.pass_match.setText("Passwords don't match!")
-            print(self.pass_1.text())
-            print(self.pass_2.text())
         else:
-            # m = sha256()
-            # m.update(bytes(self.pass_1.text + "".join([d.v for d in self.dials]), "utf8"))
-            # box = SecretBox(self._s_2_b(m.hexdigest()))
-            # q = self.file_path / (self.file_path.name + ".box")
-            # q.write_bytes(box.encrypt(self.file_path.read_bytes()))
-            self.pass_match.setText("yessir")
+            value = int("".join([str(dial.v) for dial in self.dials]))
+            key = make_key(bytes(value), self.pass_1.text())
+            encrypt_file_algo(self.file_path_str, key)
 
-    def _b_2_s(self, b):
-        return str(base64.urlsafe_b64encode(b))
-    
-    def _s_2_b(self, s):
-        return base64.urlsafe_b64decode(s)
+            # clear things up
+            self.pass_match.setText("Successfully Encrypted File")
+            self.pass_1.clear()
+            self.pass_2.clear()
+            self.file_label.setText("")
+            # reset dials so next user can't use same presets
+            for dial in self.dials:
+                dial.r1 = randrange(100)
+                dial.r2 = randrange(100)
+                dial.dial.setSliderPosition(dial.r1)
+                dial.v = (dial.r1+dial.r2)%100
+                dial.label.setText("****")
+
+
+class DecryptWidget(QWidget):
+
+    def __init__(self, num_dials=4):
+        super().__init__()
+        layout = QGridLayout()
+
+        row_1 = QHBoxLayout()
+        self.file_button = QPushButton("Choose File")
+        self.file_button.clicked.connect(self.launchDialog)
+        row_1.addWidget(self.file_button)
+        self.file_label = QLabel()
+        row_1.addWidget(self.file_label)
+
+        row_2 = QHBoxLayout()
+        self.pass_1_label = QLabel()
+        self.pass_1_label.setText("Enter Password: ")
+        row_2.addWidget(self.pass_1_label)
+        self.pass_1 = QLineEdit()
+        self.pass_1.setEchoMode(QLineEdit.Password)
+        row_2.addWidget(self.pass_1)
+        self.success_label = QLabel()
+        row_2.addWidget(self.success_label)
+
+        self.num_dials = num_dials
+        self.dials = [SecureDial() for _ in range(self.num_dials)]
+
+        row_3 = QHBoxLayout()
+        for dial in self.dials:
+            row_3.addLayout(dial.layout)
+
+        row_4 = QHBoxLayout()
+        self.encrypt_button = QPushButton("Decrypt")
+        self.encrypt_button.clicked.connect(self.runDecryption)
+        row_4.addWidget(self.encrypt_button)
+
+        layout.addLayout(row_1, 1, 1, 1, 1)
+        layout.addLayout(row_2, 2, 1, 1, 1)
+        layout.addLayout(row_3, 3, 1, 1, 1)
+        layout.addLayout(row_4, 4, 1, 1, 1)
+        self.setLayout(layout)
+
+    def launchDialog(self):
+        response = QFileDialog.getOpenFileName(
+            parent=self,
+            caption="Select a File",
+        )
+        self.file_label.setText(str(response[0]))
+        self.file_path = pathlib.Path(str(response[0]))
+        self.file_path_str = str(response[0])
+
+    def runDecryption(self):
+        value = int("".join([str(dial.v) for dial in self.dials]))
+        key = make_key(bytes(value), self.pass_1.text())
+        success = decrypt_file_algo(self.file_path_str, key)
+        if not success:
+            self.success_label.setText("Unable to Decrypt File")
+        else:
+            self.success_label.setText("Successfully Decrypted File")
+        self.file_label.setText("")
+
+        # clear things up
+        self.pass_1.clear()
+        self.file_label.setText("")
+        # reset dials so next user can't use same presets
+        for dial in self.dials:
+            dial.r1 = randrange(100)
+            dial.r2 = randrange(100)
+            dial.dial.setSliderPosition(dial.r1)
+            dial.v = (dial.r1+dial.r2)%100
+            dial.label.setText("****")
 
 
 class WelcomeWidget(QWidget):
@@ -187,10 +263,12 @@ class MainWindow(QMainWindow):
 
         self.encrypt = EncryptWidget()
         self.welcome = WelcomeWidget()
+        self.decrypt = DecryptWidget()
 
         self.tabbed = QTabWidget()
         self.tabbed.addTab(self.welcome, "Welcome")
         self.tabbed.addTab(self.encrypt, "Encrypt")
+        self.tabbed.addTab(self.decrypt, "Decrypt")
         
         self.tabbed.setDocumentMode(True)
         self.tabbed.setCurrentIndex(0)
